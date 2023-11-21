9f886cdb6fbe358ffee7c7158b5ebe2b30bcd3bd
PP2546 <iphatppp@gmail.com>
2023-11-21 16:43:05 +0700

    FINISH FINAL

--- src/Lab02.java
+++ src/Lab02.java
@@ -1,6 +1,6 @@
 public class Lab02 {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
+        // Press Alt+Enter with your caret at the highlighted design.txt to see how
         // IntelliJ IDEA suggests fixing it.
 //        Scanner s = new Scanner(System.in);
 //        System.out.println("What model do you want to use?");
